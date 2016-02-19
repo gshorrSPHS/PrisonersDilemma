4d13bb2ebad50ac521d30df0a9f30c136c5dbf59
genethebest <main@CompSci32.local>
2016-02-19 11:32:04 -0800

    team 8 changes

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -305,18 +305,16 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     ######
     ######        
     #
-    elif player == 8:
+    elif player ==8:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'pneumonoultramicroscopicsilicovolcanoconiosis'
         else:
-            # use history, opponent_history, score, opponent_score
-            # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were sucker last time
+            if len(opponent_history)==0: 
+                return 'c' 
+            elif opponent_history[-1]=='b':
+                return 'b'
             else:
-                return 'c' #otherwise collude
+                return 'c'
 
 
 
