5fcfb313e1f9206c44ed48962861e80303e92962
CrypticBacon <ldugger00@gmail.com>
2016-02-19 11:29:11 -0800

    сука блять

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -360,16 +360,14 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 10:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'сука блять'
         else:
-            # use history, opponent_history, score, opponent_score
-            # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
+            if len(opponent_history)<=1: 
+                return 'b'
+            elif opponent_history[-2:]=='cc':
+                return 'b' 
             else:
-                return 'c' #otherwise collude
+                return 'c' 
 
 
 
